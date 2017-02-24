ddb42163051b4a43174320494993e6cb40d21aad
maosea0125 <maosea0125@163.com>
2017-02-24 19:01:31 +0800

    init

---
+++ plugin/api/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ultrax`.pre_ucenter_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', '1234567890');
+define('UC_API', 'http://127.0.0.1/discuz/uc_server');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '2');
+define('UC_PPP', '20');
---
+++ plugin/api/uc_client/data/cache/apps.php
@@ -0,0 +1,40 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://127.0.0.1/discuz',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  2 => 
+  array (
+    'appid' => '2',
+    'type' => 'OTHER',
+    'name' => 'phpems',
+    'url' => 'http://127.0.0.1/3.0',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  'UC_API' => 'http://127.0.0.1/discuz/uc_server',
+);
